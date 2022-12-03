febeb3b72d6a967de7f5e8c5eb0599666fb03fe8
Alik <alikharutyunyan00@gmail.com>
2022-12-03 02:44:54 +0200

    Created a break point for the loop after finding duplicates + changed texts to make the game more entertaining

--- src/Exercise7.java
+++ src/Exercise7.java
@@ -27,7 +27,7 @@ public class Exercise7 {
         Scanner scanner = new Scanner(System.in);
         int codeToGuess;
         boolean continuePlaying = true;
-        System.out.println("Guess the secret code: ");
+        System.out.println("LET'S PLAY 'GUESS THE SECRET CODE' You need to guess 4 numbers from 1 to 6 (NO DUPLICATES!)");
         int numberOfRoute = chooseDifficulty();
         int maxTries = calculateTries(numberOfRoute);
 
@@ -35,21 +35,21 @@ public class Exercise7 {
 
             if (maxTries > 0) {
                 if (numberOfRoute != 4) {
-                    System.out.println("you have " + maxTries + " tries left");
+                    System.out.println("You have " + maxTries + " tries left");
                 } else {
-                    System.out.println("you have surprise tries left");
+                    System.out.println("You have SURPRISE tries left");
                 }
 
                 codeToGuess = scanner.nextInt();
 
-                if (hasDublicates(codeToGuess)) {
-                    System.out.println("you got fined by 2 tries");
+                if (hasDuplicates(codeToGuess)) {
+                    System.out.println("!!! YOU GOT FINED BY 2 POINTS, DON'T REPEAT NUMBERS !!!");
                     maxTries-=2;
                 }
 
                 if (String.valueOf(codeToGuess).length() == 4) {
                     if (randomNumber == codeToGuess) {
-                        System.out.println("Congratulations! you won the game");
+                        System.out.println("Congratulations! You won the game");
                         continuePlaying = false;
                     } else {
                         getHint(codeToGuess, randomNumber);
@@ -58,7 +58,7 @@ public class Exercise7 {
             }
 
             else {
-                System.out.println("you dont have any tries left");
+                System.out.println("!!!GAME OVER!!! You dont have any tries left");
                 continuePlaying=false;
             }
 
@@ -71,6 +71,7 @@ public class Exercise7 {
         String codeToGuessString = "" + codeToGuess;
         String randomNumberString = "" + randomNumber;
 
+        System.out.println("------------------------------------");
         for (int i=0; i<codeToGuessString.length(); i++) {
             if (randomNumberString.contains("" + codeToGuessString.charAt(i))) {
                 if (randomNumberString.charAt(i)==codeToGuessString.charAt(i)) {
@@ -82,6 +83,7 @@ public class Exercise7 {
                 }
             }
         }
+        System.out.println("------------------------------------");
     }
 
     public static int chooseDifficulty() {
@@ -91,11 +93,11 @@ public class Exercise7 {
         boolean endLoop = false;
 
         do {
-            System.out.println("choose the the route:");
-            System.out.println("1. easy route - 20 tries");
-            System.out.println("2. medium route - 15 tries");
-            System.out.println("3. hard route - 10 tries");
-            System.out.println("4. surprise route - 5-25 tries");
+            System.out.println("Choose the the route:");
+            System.out.println("1. Easy route - 20 tries");
+            System.out.println("2. Medium route - 15 tries");
+            System.out.println("3. Hard route - 10 tries");
+            System.out.println("4. Surprise route - 5-25 tries");
             userInput = scanner.nextInt();
 
             if (userInput>=1 && userInput<=4) {
@@ -129,7 +131,7 @@ public class Exercise7 {
         return tries;
     }
 
-    public static boolean hasDublicates(int codeToGuess) {
+    public static boolean hasDuplicates(int codeToGuess) {
         boolean hasDuplicates = false;
         String codeToGuessString = codeToGuess + "";
 
@@ -137,6 +139,7 @@ public class Exercise7 {
             for (int j = i+1; j < codeToGuessString.length(); j++) {
                 if (codeToGuessString.charAt(i)==codeToGuessString.charAt(j)) {
                     hasDuplicates = true;
+                    break;
                 }
             }
         }
