e3214aeb0f54764393dfcee937be7710e790cd04
michelleschneider <xmichelle455@gmail.com>
2022-12-04 21:09:14 +0200

    initial commit

--- src/Exercise6.java
+++ src/Exercise6.java
@@ -7,6 +7,12 @@ public class Exercise6 {
         System.out.println("Enter your quadratic equation: ");
         userInput = scanner.nextLine();
         System.out.println(isQuadraticEquation(userInput));
+        if (isQuadraticEquation(userInput)) {
+            int a = getA(userInput);
+            int b = getB(userInput);
+            int c = getC(userInput);
+           solveQuadraticEquation(a,b,c);
+        }
     }
 
     public static boolean isNumber(char toCheck){
@@ -81,5 +87,65 @@ public class Exercise6 {
             }
         }
         return result;
+    }
+
+    public static int getA (String userInput) {
+        int result;
+        if (userInput.startsWith("-x")) {
+            result = -1;
+        }
+
+        else if (userInput.startsWith("x")) {
+            result = 1;
+        }
+
+        else {
+            result = Integer.parseInt(userInput.substring(0,userInput.indexOf("x")));
+        }
+        return result;
+    }
+
+    public static int getB (String userInput) {
+        int result;
+        userInput = userInput.substring(userInput.indexOf("x^2")+3);
+        userInput = userInput.replaceAll(" ","");
+
+        if (userInput.startsWith("-x") || userInput.startsWith("+x")) {
+            if (userInput.charAt(0)== '-') {
+                result = -1;
+            }
+
+            else {
+                result = 1;
+            }
+        }
+
+        else {
+            result = Integer.parseInt(userInput.substring(0,userInput.indexOf("x")));
+        }
+        return result;
+    }
+
+    public static int getC (String userInput) {
+        userInput = userInput.substring(userInput.lastIndexOf("x")+1, userInput.indexOf("="));
+        userInput = userInput.replaceAll(" ","");
+        return Integer.parseInt(userInput);
+    }
+
+    public static void solveQuadraticEquation(int firstParameter, int secondParameter, int thirdParameter){
+        double firstAnswer, secondAnswer;
+        double quadEquationPart = Math.sqrt(secondParameter * secondParameter - 4 * firstParameter * thirdParameter);
+        firstAnswer = (-secondParameter + quadEquationPart)/2*firstParameter;
+        secondAnswer = (-secondParameter - quadEquationPart)/2*firstParameter;
+
+        if(quadEquationPart >= 0){
+            if(firstAnswer != secondAnswer) {
+                System.out.println("Solved! There are 2 answers: x1 = " + firstAnswer + " x2 = " + secondAnswer);
+            }else{
+                System.out.println("Solved! There is one answer: x1 = " + firstAnswer);
+            }
+        }else {
+            System.out.println("Unfortunately, there is no answer!");
+        }
     }
 }
