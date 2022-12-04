f730a2debba85277a80c58572127493993de5d36
Alik <alikharutyunyan00@gmail.com>
2022-12-04 16:29:22 +0200

    Change the code to work with spaces and used function isNumber

--- src/Exercise6.java
+++ src/Exercise6.java
@@ -9,99 +9,77 @@ public class Exercise6 {
         System.out.println(isQuadraticEquation(userInput));
     }
 
-    public static boolean isQuadraticEquation (String userInput) {
+    public static boolean isNumber(char toCheck){
+        boolean isNum = false;
+        for (int j = 0; j < 10; j++) {
+            if ((toCheck + "").equals(j + "")) {
+                isNum = true;
+                break;
+            }
+        }
+        return isNum;
+    }
+
+    public static boolean isQuadraticEquation(String userInput){
+        boolean result = false;
         boolean firstPart = false;
         boolean secondPart = false;
-        boolean result = false;
 
-        if (userInput.contains("x^2")) {
-            if (userInput.charAt(0)=='-') {
+        if(userInput.contains("x^2")) {
+            if(userInput.charAt(0) == '-'){
                 userInput = userInput.substring(1);
             }
-            if (userInput.indexOf("x^2")!=0) {
-                for (int j = 0; j < userInput.indexOf("x^2"); j++) {
-                    for (int k = 0; k < 10; k++) {
-                        if (("" + userInput.charAt(j)).equals("" + k)) {
-                            firstPart = true;
-                            break;
-                        }
-
-                        else {
-                            firstPart = false;
-                        }
-                    }
-
+            if (userInput.indexOf("x^2") != 0) {
+                for(int i = 0; i < userInput.indexOf("x^2"); i++) {
+                    firstPart = isNumber(userInput.charAt(i));
                     if (!firstPart) {
                         break;
                     }
                 }
+            }else{
+                firstPart = true;
             }
-
-            else {
-                firstPart=true;
-            }
-
         }
 
-        if (firstPart) {
-            userInput=userInput.substring(userInput.indexOf("x^2")+3);
-
-            if (userInput.charAt(0) == '+' || userInput.charAt(0) == '-') {
-                userInput = userInput.substring(1);
-                if (userInput.contains("x")) {
-                    if (userInput.indexOf("x")!=0) {
-                        for (int i = 0; i < userInput.indexOf("x"); i++) {
-                            for (int j = 0; j < 10; j++) {
-                                if (("" + userInput.charAt(i)).equals("" + j)) {
-                                    secondPart = true;
+        if(firstPart){
+            userInput = userInput.substring(userInput.indexOf("x^2") + 3).trim();
+            if(userInput.length() > 1) {
+                if (userInput.charAt(0) == '+' || userInput.charAt(0) == '-') {
+                    userInput = userInput.substring(1).trim();
+                    if (userInput.contains("x")) {
+                        if(userInput.indexOf("x") != 0) {
+                            for (int i = 0; i < userInput.indexOf("x"); i++) {
+                                secondPart = isNumber(userInput.charAt(i));
+                                if (!secondPart) {
                                     break;
                                 }
-
-                                else {
-                                    secondPart = false;
+                            }
+                        }else{
+                            secondPart = true;
+                        }
+                    }
+                }
+            }
+        }
+        if(secondPart){
+            userInput = userInput.substring(userInput.indexOf("x") + 1).trim();
+            if(userInput.length() > 1){
+                if (userInput.charAt(0) == '+' || userInput.charAt(0) == '-') {
+                    if(userInput.endsWith("0") && userInput.contains("=")){
+                        userInput = userInput.substring(1,userInput.length()-1).trim();
+                        if(userInput.endsWith("=")) {
+                            userInput = userInput.replaceFirst("=","");
+                            for (int i = 0; i < userInput.trim().length(); i++) {
+                                result = isNumber(userInput.charAt(i));
+                                if (!result) {
+                                    break;
                                 }
                             }
-
-                            if (!secondPart) {
-                                break;
-                            }
-                        }
-                    }
-
-                    else {
-                        secondPart = true;
-                    }
-                }
-            }
-        }
-
-        if (secondPart) {
-            userInput = userInput.substring(userInput.indexOf("x")+1);
-            if (userInput.charAt(0) == '+' || userInput.charAt(0) == '-') {
-                userInput = userInput.substring(1);
-
-                if (userInput.endsWith("=0")) {
-                    for (int i = 0; i < userInput.indexOf("=0"); i++) {
-                        for (int j = 0; j < 10; j++) {
-
-                            if (("" + userInput.charAt(i)).equals("" + j)) {
-                                result = true;
-                                break;
-                            }
-
-                            else {
-                                result = false;
-                            }
-                        }
-
-                        if (!result) {
-                            break;
                         }
                     }
                 }
             }
         }
-
         return result;
     }
 }
